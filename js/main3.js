3ab7fc5998678d4021ed64af50e4e5db3281fa01
CarmineVitale <carmine.vitale90@gmail.com>
2020-04-03 17:04:33 +0200

    Esercizio 3 e 4 completati (da inserire funzione)

--- js/main3.js
+++ js/main3.js
@@ -1,1 +1,17 @@
 console.log('esercizio 3');
+var nomi = ['Michele' , 'Fabio', 'Roberto'];
+var cognomi = ['Forghieri', 'Papagni', 'Marazzini'];
+
+ var listaFalsa = [];
+
+ for (var i = 0; i < 5; i++) {
+    var nomeNumber = Math.floor(Math.random() * ((nomi.length - 1) + 1) + 0);
+    console.log(nomeNumber);
+    
+    var cognomeNumber = Math.floor(Math.random() * ((nomi.length - 1) + 1) + 0); 
+    console.log(cognomeNumber);
+    var newName = nomi[nomeNumber] + ' ' + cognomi[cognomeNumber];
+
+    listaFalsa.push(newName);
+}
+console.log(listaFalsa);
--- js/main4.js
+++ js/main4.js
@@ -1,21 +1,14 @@
 var numeri = [1, 4, 2, 15, 120, 17, 3];
 console.log(numeri);
 
-var posizioneDispari = [];
+var somma = 0;
 
-for (var i = 0; i< numeri.length; i= i + 2) {
-    posizioneDispari.push(numeri[i])
+for (var i = 1; i< numeri.length; i= i + 2) {
+    somma += numeri[i];
 }
-console.log(posizioneDispari);
 
-for (var i = 0; i < posizioneDispari.length ; i++) {
-    var somma = 0;
-    somma += posizioneDispari[i];
-}
+
 console.log('la somma dei numeri messi in posizione dispari è',somma); 
-
-
-
 
 /* var somma = posizioneDispari[0] + posizioneDispari[1] + posizioneDispari[2] + posizioneDispari[3];
 console.log(somma); */
