0966da83d263b5404da8e0e78553f725d2a4fa83
CarmineVitale <carmine.vitale90@gmail.com>
2020-04-03 18:04:59 +0200

    Inserita risoluzione anche con funzioni all'esercizio 3 e 4

--- js/main3.js
+++ js/main3.js
@@ -3,7 +3,7 @@ var nomi = ['Michele' , 'Fabio', 'Roberto'];
 var cognomi = ['Forghieri', 'Papagni', 'Marazzini'];
 
  var listaFalsa = [];
-
+/*
  for (var i = 0; i < 5; i++) {
     var nomeNumber = Math.floor(Math.random() * ((nomi.length - 1) + 1) + 0);
     console.log(nomeNumber);
@@ -15,3 +15,18 @@ var cognomi = ['Forghieri', 'Papagni', 'Marazzini'];
     listaFalsa.push(newName);
 }
 console.log(listaFalsa);
+*/
+//Con funzione 
+
+//invocazione funzione nel ciclo for
+for (var i = 0; i < 5; i++) {
+    listaFalsa.push(nomi[randomNumber(0, nomi.length)] + ' ' + cognomi[randomNumber(0, cognomi.length)] ); 
+}
+console.log(listaFalsa);
+
+
+
+//Funzione
+function randomNumber (min, max) {
+    return Math.floor(Math.random()* (max - min)) + min;
+}
--- js/main4.js
+++ js/main4.js
@@ -3,12 +3,43 @@ console.log(numeri);
 
 var somma = 0;
 
-for (var i = 1; i< numeri.length; i= i + 2) {
+//Risoluzione senza funzione
+
+/*for (var i = 1; i< numeri.length; i= i + 2) {
     somma += numeri[i];
 }
 
 
-console.log('la somma dei numeri messi in posizione dispari è',somma); 
+console.log('la somma dei numeri messi in posizione dispari è',somma); */
 
-/* var somma = posizioneDispari[0] + posizioneDispari[1] + posizioneDispari[2] + posizioneDispari[3];
-console.log(somma); */
+
+//Con funzione
+
+//invocazione funzione in ciclo for
+for (var i = 0; i < numeri.length; i++) {
+    if (isDispari(i)) {
+        somma += numeri[i];
+    }
+} 
+console.log('La somma dei numeri messi in posizione dispari è', somma);
+
+
+
+var ciao = 0;
+for (var i = 0; i < numeri.length; i++) {
+    if (isPari(i)) {
+        ciao += numeri[i];
+    }
+}
+console.log('La somma dei numeri messi in posizione pari è', ciao);
+//Funzioni
+function isDispari(num) {
+    var ilnumeroinseritoedispari = num % 2 !== 0;
+   //ritornerà true se il numero è dispari
+   return ilnumeroinseritoedispari;
+}
+
+function isPari(num) {
+    var pippo = num % 2 == 0;
+    return pippo;
+}
